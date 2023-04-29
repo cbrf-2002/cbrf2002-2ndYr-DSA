2524826f357c6d634b71713bd0e0009fd9c0b177
cbrf-2002 <78638777+cbrf-2002@users.noreply.github.com>
2023-04-30 01:38:08 +0800

    Create Discussion 1.py

---
+++ 1st Year/CPE-105 Python/Py/Discussion 1.py
@@ -0,0 +1,6 @@
+name = input("Input your name: ")
+dept = input("Input your department: ")
+age = str(input("Input your age: "))
+address = input("Input your address: ")
+
+print("\nMy name is " + name + " from " + dept + " department.\nI'm " + age + " years old and my address is " + address + ".")
