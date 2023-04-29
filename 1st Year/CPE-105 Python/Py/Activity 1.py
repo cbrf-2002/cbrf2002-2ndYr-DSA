232ef4ef46482cae0101b2aaf317b4d6913441c2
cbrf-2002 <78638777+cbrf-2002@users.noreply.github.com>
2023-04-30 01:33:22 +0800

    Create Activity 1.py

---
+++ 1st Year/CPE-105 Python/Py/Activity 1.py
@@ -0,0 +1,6 @@
+Name = "Charles Bryan R. Fabian"
+Dept = "Computer Engineering"
+Age = 19
+Address = "Tagaytay, Cavite"
+
+print ("My name is " + Name + " from " + Dept + "\nI'm " + str(Age) + " and my address is " + Address)
