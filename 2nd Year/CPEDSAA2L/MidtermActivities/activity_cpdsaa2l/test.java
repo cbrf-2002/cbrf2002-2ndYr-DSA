f4f1f0110ea6ec5c786722b418315400ae5e8fc2
Charles Bryan Retaga Fabian <78638777+cbrf2002@users.noreply.github.com>
2023-05-02 13:35:10 +0800

    Add files via upload

--- 2nd Year/CPEDSAA2L/MidtermActivities/activity_cpdsaa2l/test.java
+++ 2nd Year/CPEDSAA2L/MidtermActivities/activity_cpdsaa2l/test.java
@@ -1,5 +1,7 @@
 package cbrf2002.activity_cpdsaa2l;
 
+import java.util.Arrays;
+
 /*
  *
  * @author cbrf2002 | Charles Bryan Retaga Fabian
@@ -9,6 +11,12 @@ package cbrf2002.activity_cpdsaa2l;
 
 public class test {
     public static void main(String[] args) {
+        int[] aasa = {2,1,4,8,9,5,67,43,8,9,0};
+        Arrays.parallelSort(aasa);
         
+        System.out.println("Parr sort: ");
+        for (int val : aasa) {
+            System.out.print(val + ", "); 
+        }
     }
 }
