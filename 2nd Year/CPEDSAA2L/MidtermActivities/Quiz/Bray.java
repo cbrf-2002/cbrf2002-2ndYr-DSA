5da099d0dfe751d99aa41f81e8713c2e8a66736b
Charles Bryan Retaga Fabian <78638777+cbrf2002@users.noreply.github.com>
2023-05-02 15:50:10 +0800

    Update and rename Quiz_1.java to Bray.java

---
+++ 2nd Year/CPEDSAA2L/MidtermActivities/Quiz/Bray.java
@@ -0,0 +1,11 @@
+package activity;
+//Fabian, Charles Bryan R. - CPE - 211
+public class Bray {
+    public static void main(String[] args) {
+        String mousepad[] = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+        System.out.println("Index   "+"Value(Element)");
+        for (int u = 0; u < mousepad.length; u++){
+            System.out.println(u+"       "+mousepad[u]);
+        }
+    }
+}
