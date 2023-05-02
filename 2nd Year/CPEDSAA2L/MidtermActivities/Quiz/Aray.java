7a2cba7f4b7a53a595d9243360890dffa3b0fa16
Charles Bryan Retaga Fabian <78638777+cbrf2002@users.noreply.github.com>
2023-05-02 15:49:34 +0800

    Create Aray.java

---
+++ 2nd Year/CPEDSAA2L/MidtermActivities/Quiz/Aray.java
@@ -0,0 +1,11 @@
+package Quiz;
+//Fabian, Charles Bryan R. - CPE - 211
+public class Aray {
+    public static void main(String[] args) {
+        int mousepad[] = {32,12,18,54,2};
+        System.out.println("Index   "+"Value(Element)");
+        for (int u = 0; u < mousepad.length; u++){
+            System.out.println(u+"       "+mousepad[u]);
+        }
+    }
+}
