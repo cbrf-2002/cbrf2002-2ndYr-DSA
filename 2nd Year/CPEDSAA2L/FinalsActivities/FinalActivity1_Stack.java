832fe4235d0f612565f22502b097cb23489c238e
cbrf2002 <charlesbryan.fabian10@gmail.com>
2023-05-20 01:19:26 +0800

    Modified to shorten - Especially with switch()

--- 2nd Year/CPEDSAA2L/FinalsActivities/FinalActivity1_Stack.java
+++ 2nd Year/CPEDSAA2L/FinalsActivities/FinalActivity1_Stack.java
@@ -14,47 +14,37 @@ public class FinalActivity1_Stack {
             mainMenu();
             System.out.print("\nDo you want to continue? (y to continue, press any other keys to end): ");
             conter = scanner.next().charAt(0);
-        } while (conter == 'y' || conter == 'Y');
+        } while (Character.toLowerCase(conter) == 'y');
         System.out.println("\nDone! Thank you for using!");
         scanner.close();
     }
-    public static boolean mainMenu() {
-        System.out.print("\nChoose any number (1-4) from the following operations:");
-        System.out.print("\n\t1. Display the Original Elements of the Stack");
-        System.out.print("\n\t2. Insert an element");
-        System.out.print("\n\t3. Pop an element");
-        System.out.print("\n\t4. Peek of the stack");
-        System.out.print("\nEnter here: ");
+    public static void mainMenu() {
+        System.out.print("\nChoose a number (1-4) for the following operations:\n" +
+                "\t1. Display the Original Elements of the Stack\n" +
+                "\t2. Insert an element\n" +
+                "\t3. Pop an element\n" +
+                "\t4. Peek of the stack\n" +
+                "Enter here: ");
         int choice = scanner.nextInt();
         switch (choice) {
-            case 1:
-                displaying();
-                return true;
-            case 2:
-                inserting();
-                return true;
-            case 3:
-                popping();
-                return true;
-            case 4:
-                peeker();
-                return true;
-            default:
-                System.out.println("Invalid input, please try again.\n");
-                return false;
+            case 1 -> displaying();
+            case 2 -> inserting();
+            case 3 -> popping();
+            case 4 -> peeker();
+            default -> System.out.println("Invalid input, please try again.\n");
         }
     }
     public static void displaying() {
         if (!stackElement.isEmpty()) {
             System.out.println("\nOriginal Stack List: " + stackElementOriginal);
         } else {
-            System.out.println("\nOriginal Stack List: Is empty...");
+            System.out.println("\nOriginal Stack List is empty...");
         }
     }
     public static void inserting() {
         System.out.print("\nEnter an integer to insert in stack: ");
-        int insertion = scanner.nextInt();
-        stackElement.push(insertion);
+        int insertedElement = scanner.nextInt();
+        stackElement.push(insertedElement);
         System.out.println("\nNEW stack list: " + stackElement);
     }
     public static void popping() {
